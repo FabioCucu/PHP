ccdec614bf4821bbdbc02daac418b455f9f6a7a7
FabioCucu <122224459+FabioCucu@users.noreply.github.com>
2025-12-12 12:57:41 +0100

    Read File & explode()

--- IntroUDA4/index.php
+++ IntroUDA4/index.php
@@ -217,17 +217,31 @@ $classe = [
 ];
 
 $file = fopen("voto.txt", "w");
-foreach($classe as $stud)
+foreach($classe as $keys => $stud)
     {
-        $line = implode(",", $stud).PHP_EOL;
-        fwrite($file,$line);
+        $line = $keys." = ".implode(",", $stud).PHP_EOL;//implode -> Array in stringa
+        fwrite($file,$line);//File grandi, in alternativa, usare file_put_contents
     }
+
+// ============================ Lettura su file ============================
+
+$datiFromFile = [];
+$file = fopen("voto.txt", "r");
+while(($line = fgets($file)) !== false) { // Similmente a fwrite <=> file_put_contents, abbiamo file_get_contents
+    $datiFromFile = trim($line);
+}
 fclose($file);
+foreach($datiFromFile as $dati) {
+    echo $dati."<br>";
+}
+echo $datiFromFile[1]."<br>";
 
-$file = fopen("voto.txt", "w");
-foreach($classe as $stud)
-    {
-        $line = = array_keys($studente).".".implode(",", $stud).PHP_EOL;
-        fwrite($file,$line);//File grandi, in alternativa, usare file_put_contents()
-    }
+echo "<br><br>";
 
+// ============================ Explode stringa in array ============================
+
+$Stringa = "The moon is red tonight. You shall know death. - (By Remilia)";
+$arrayFrase = explode(" ", $Stringa);
+foreach($arrayfrase as $parola) {
+    echo $parola."<br>";
+}
